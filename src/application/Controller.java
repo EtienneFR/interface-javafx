3928d21b412c7bcf90d4c7524a3fd9a0516f3db7
Etienne Dayre <etiennedayre10@gmail.com>
2021-01-26 23:14:31 +0100

    feat: add values in ComboBox and ChoiceBox and button for quite application

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,16 +1,18 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class Controller {
 
@@ -18,25 +20,25 @@ public class Controller {
     private Button newTypologyBtn;
 
     @FXML
-    private TextField date;
+    private DatePicker date;
     
     @FXML
-    private ChoiceBox<?> typology;
+    private ChoiceBox<String> typology;
 
     @FXML
-    private ChoiceBox<?> subType;
+    private ChoiceBox<String> subType;
 
     @FXML
-    private ChoiceBox<?> subSubType;
+    private ChoiceBox<String> subSubType;
 
     @FXML
     private ComboBox<String> frequency;
 
     @FXML
-    private ComboBox<?> severity;
+    private ComboBox<String> severity;
 
     @FXML
-    private ComboBox<?> nonDetectability;
+    private ComboBox<String> nonDetectability;
 
     @FXML
     private TextArea comment;
@@ -90,13 +92,35 @@ public class Controller {
     private Button deleteIncidentBtn;
 
     @FXML
+    private Button btnExit;
+
+    @FXML
     private Label errorLabel;
+
+    @FXML
+    void Quit(ActionEvent event) {
+    	Platform.exit();
+
+    }
+    
+    private String[] typologyValues = { "Type 1", "Type 2", "Type 3" };
+    private String[] subTypeValues = { "A1", "A2", "A3" }; 
+    private String[] subSubTypeValues = { "Général", "Partiel" }; 
     
     private String[] frequencyValues = { "X1", "X2", "X3", "X10", "X20" }; 
+    private String[] severityValues = { "Peu importante", "Moyenne", "Importante", "Très importante"};
+    private String[] nonDetectabilityValues = { "Avant réception", "Après réception" };
+    
     
     @FXML
     public void initialize() {
+    	typology.setItems(FXCollections.observableArrayList(typologyValues));
+    	subType.setItems(FXCollections.observableArrayList(subTypeValues));
+    	subSubType.setItems(FXCollections.observableArrayList(subSubTypeValues));
+    	
         frequency.setItems(FXCollections.observableArrayList(frequencyValues));
+        severity.setItems(FXCollections.observableArrayList(severityValues));
+        nonDetectability.setItems(FXCollections.observableArrayList(nonDetectabilityValues));
     }
 
 }
